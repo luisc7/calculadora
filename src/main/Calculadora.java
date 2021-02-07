eeb65754b6cd9e5dc424aee556502ec47a53ee12
Luis Cifuentes <luiscifuentes7@gmail.com>
2021-02-07 15:53:22 +0100

    Modificar multiplicacion en main

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -5,6 +5,9 @@ import operaciones.Resta;
 public class Calculadora {
 
 	public static void main(String[] args) {
+		
+		// MULTIPICACION
+		System.out.println("-----MULTIPLICACION-------");
 		float r2 = 0F;
 		float p1 = 3.1F;
 		float p2 = 2.5F;
@@ -12,30 +15,31 @@ public class Calculadora {
 		int p1e = 4;
 		int p2e = 17;
 		int r2e = 0;
-		
-		
+
+		System.out.print("MultiplicaciÃ³n de nÃºmeros reales " + p1 + " por " + p2 + " = ");
 		r2 = Multiplicacion.mult2Reales(p1, p2);
 		System.out.println(r2);
-		
+
+		System.out.print("MultiplicaciÃ³n de los nÃºmeros enteros " + p1e + " por " + p2e + " = ");
 		r2e = Multiplicacion.mult2Enteros(p1e, p2e);
 		System.out.println(r2e);
-		
-		
+		System.out.println("-----FIN-MULTIPLICACION-------\n");
+				
 		
 		//Creo los objetos para realizar las distintas operaciones
 		Resta rest1 = new Resta(3,2);
 		Resta rest2 = new Resta(5,3,8);
 		Resta rest3 = new Resta(3);
 				
-		//Realizamos las operaciones haciendo las distintas llamdas a los métodos de la clase
-		System.out.print("Resultado de la resta de dos números reales: ");
+		//Realizamos las operaciones haciendo las distintas llamdas a los mï¿½todos de la clase
+		System.out.print("Resultado de la resta de dos nï¿½meros reales: ");
 		rest1.calcularDosReales();
-		System.out.print("Resultado de la resta de dos números enteros: ");
+		System.out.print("Resultado de la resta de dos nï¿½meros enteros: ");
 		rest1.calcularDosEnteros();
-		System.out.print("Resultado de la resta de tres números reales: ");
+		System.out.print("Resultado de la resta de tres nï¿½meros reales: ");
 		rest2.calcularTresReales();
 				
-		//Aquí realizo la resta de valores que se van introduciendo
+		//Aquï¿½ realizo la resta de valores que se van introduciendo
 		System.out.print("\nMostramos el primer valor a restar:");
 		rest3.stringMostrarValor();
 		System.out.print("El resultado de la resta es:");
