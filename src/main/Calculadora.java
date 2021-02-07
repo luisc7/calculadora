c450a85de36633c1de286c598b217e865787b2c3
DeiMoS-es <nagib.86@gmail.com>
2021-02-07 15:22:34 +0100

    añado el main

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -1,5 +1,6 @@
 package main;
 import operaciones.Multiplicacion;
+import operaciones.Resta;
 
 public class Calculadora {
 
@@ -18,6 +19,33 @@ public class Calculadora {
 		
 		r2e = Multiplicacion.mult2Enteros(p1e, p2e);
 		System.out.println(r2e);
+		
+		
+		
+		//Creo los objetos para realizar las distintas operaciones
+		Resta rest1 = new Resta(3,2);
+		Resta rest2 = new Resta(5,3,8);
+		Resta rest3 = new Resta(3);
+				
+		//Realizamos las operaciones haciendo las distintas llamdas a los métodos de la clase
+		System.out.print("Resultado de la resta de dos números reales: ");
+		rest1.calcularDosReales();
+		System.out.print("Resultado de la resta de dos números enteros: ");
+		rest1.calcularDosEnteros();
+		System.out.print("Resultado de la resta de tres números reales: ");
+		rest2.calcularTresReales();
+				
+		//Aquí realizo la resta de valores que se van introduciendo
+		System.out.print("\nMostramos el primer valor a restar:");
+		rest3.stringMostrarValor();
+		System.out.print("El resultado de la resta es:");
+		rest3.calcRestAcu();
+		rest3.setN4(5);
+		System.out.print("\nMostramos el segundo valor a restar:");
+		rest3.stringMostrarValor();
+		System.out.print("El resultado de la resta es:");
+		rest3.calcRestAcu();
 	}
+	
 
 }
