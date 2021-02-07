cbb1c37916fc887d64e7347a870b1acd87ad777c
Luis Cifuentes <luiscifuentes7@gmail.com>
2021-02-07 11:44:53 +0100

    Primer commit

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"calculadora","l":"cociente"},{"p":"calculadora","l":"Multiplicacion"},{"p":"calculadora","l":"resta"},{"p":"calculadora","l":"Suma"}];updateSearchResults();
