ed2635568d0c90d1190cc0931c15a2f1edac8429
DeiMoS-es <nagib.86@gmail.com>
2021-02-07 15:03:36 +0100

    creo los constructores de la clase

--- src/operaciones/Resta.java
+++ src/operaciones/Resta.java
@@ -1,5 +1,59 @@
+/**
+ * Esta clase representa la clase resta de una calculadora que realiza las 4 operaciones básicas
+ * @author Nagib Delgado
+ * @since 29/02/2021
+ * @version 0.1
+ * 
+ * 	Resta de dos números reales, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+	Resta de dos números enteros, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+	Resta de tres números reales, tendrá 3 parámetros de entrada y uno de salida que será la solución.
+	Resta con valor acumulado, tendrá un parámetro de entrada y la clase deberá de guardar el valor acumulado.
+
+ */
 package operaciones;
 
 public class Resta {
+	/**
+	 * Zona definicion de atributos
+	 * 
+	 */
+	private double n1;
+	private double n2;
+	private double n3;
+	private int n4;
+	private int acu =0;
+	
+	//Zona constructores
+	
+	/**
+	 * Este constructor es el constructor que realizará la operación resta de dos números reales y enteros
+	 * @param n1 representa el primer número a restar
+	 * @param n2 representa el segundo número a restar
+	 */
+	public Resta(double n1, double n2) {
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+	
+	/**
+	 * Este constructor es el constructor que realizará la operación resta de tres números reales
+	 * @param n1 representa el primer número a restar
+	 * @param n2 representa el segundo número a restar
+	 * @param n3 representa el tercer número a restar
+	 */
 
+	public Resta(double n1, double n2, double n3) {
+		this.n1 = n1;
+		this.n2 = n2;
+		this.n3 = n3;	
+	}	
+	
+	/**
+	 * Este es el constructor para almacenar el valor introducido para luego utilizarlo para ir restando.
+	 * @param n4 representa el valor introducido que se va a ir almacenando
+	 */
+	
+	public  Resta ( int  n4 ) {
+		this.n4 = n4;
+	}
 }
