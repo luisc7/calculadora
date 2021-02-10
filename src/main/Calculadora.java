1b878c7d6d5e3010b07e3e650892a308ae2c55d5
Ana <anabelenariasdiaz@gmail.com>
2021-02-10 17:53:11 +0100

    añadido al repositorio comun suma

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -115,6 +115,38 @@ public class Calculadora {
 		res4 = dividir.raiz(fd1);
 		System.out.println(res4);
 
+		/**
+		 * ------------------------------
+		 * |							|
+		 * |			SUMA		|
+		 * |							|
+		 * ------------------------------
+		 */
+		
+		//COMIENZO SUMA
+		
+		System.out.println("-------SUMA------");
+				
+				float n1= 1.2f;
+				float n2= 2.5f;
+				float n3= 3.6f;
+				float R2= 0f;
+				float R3= 0f;
+				int n1e= 5;
+				int n2e= 6;
+				int E2=0;
+				
+				System.out.println("La suma de dos numeros reales es: ");
+				R2= Suma.sumaDosReales(n1, n2);
+				System.out.println(R2);
+				
+				System.out.println("La suma de dos numeros enteros es: ");
+				E2= Suma.sumaDosEnteros(n1e, n2e);
+				System.out.println(E2);
+				
+				System.out.println("La suma de tres numeros reales es: ");
+				R3= Suma.sumaTresReales(n1, n2, n3);
+				System.out.println(R3);
 		
 	}
 	
