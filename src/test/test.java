79bdf73ce24fdd8bdfc51ac8381e77d8924d8ec7
David Fernandez <david.fernandez.peralta@gmail.com>
2021-02-28 20:43:54 +0100

    DS STORE

---
+++ src/test/test.java
@@ -0,0 +1,22 @@
+package test;
+import operaciones.Resta;
+
+/**
+ * Esta clase contien las pruebas unitarias de Sumar
+ * @author david Fernandez Peralta
+ * @since 25/02/2021
+ * @version 1.0
+ *
+ */
+
+public class test {
+
+	
+	
+	@Test
+	void testRestaDos() {
+		Resta r1 = new Resta;
+		
+		assertEquals(5, r1.r);
+	}
+}
